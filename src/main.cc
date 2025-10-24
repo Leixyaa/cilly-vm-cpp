beb72af039848c768045f1da3a7e9f11365f1a9d
Leixyaa <1959920806@qq.com>
2025-10-24 20:51:22 +0800

    feat: initialize VS2022 project and first run test

---
+++ src/main.cc
@@ -0,0 +1,11 @@
+// cilly-vm-cpp
+// Author: Leixyaa
+// Date: 10.24
+// Description: Entry point for testing environment.
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, cilly VM world" << std::endl;
+	return 0;
+}
