b8c179226b76e72de04a29d0d931af16f128424b
Leixyaa <1959920806@qq.com>
2025-12-11 14:39:13 +0800

    前端：实现生成器基础逻辑，支持 print + 数字表达式

---
+++ src/frontend/generator.cc
@@ -0,0 +1,113 @@
+#include "generator.h"
+
+namespace cilly {
+
+Generator::Generator() : current_fn_(nullptr) {}
+
+Function Generator::Generate(const std::vector<StmtPtr>& program) {
+  Function script;
+  script.SetLocalCount(0);
+  current_fn_ = &script;
+  for (const auto& i : program) {
+    EmitStmt(i);
+  }
+  EmitOp(OpCode::OP_RETURN);
+  current_fn_ = nullptr;
+  return script;
+}
+
+void Generator::EmitStmt(const StmtPtr& stmt) {      // 分类处理不同类型语句
+  switch (stmt->kind) {
+  case Stmt::Kind::kPrint: {
+      auto p = static_cast<PrintStmt*>(stmt.get());  // get()拿出unique_ptr中的原始指针stmt*
+      EmitPrintStmt(p);                              // get只是借出对象，所有权依旧只有stmt，stmt离开作用于后p依旧会失效
+      break;
+    }
+  case Stmt::Kind::kExpr: {
+      auto p = static_cast<ExprStmt*>(stmt.get());
+      EmitExprStmt(p);
+      break;
+    }
+  }
+}
+
+void Generator::EmitPrintStmt(const PrintStmt* stmt) {
+  EmitExpr(stmt->expr);
+  EmitOp(OpCode::OP_PRINT);
+  return;
+}
+
+void Generator::EmitExprStmt(const ExprStmt* stmt) {
+  EmitExpr(stmt->expr);
+  EmitOp(OpCode::OP_POP);
+  return;
+}
+
+void Generator::EmitExpr(const ExprPtr& expr) {   // 分类处理不同类型表达式
+  switch (expr->kind) {
+  case Expr::Kind::kLiteral: {
+    auto p = static_cast<LiteralExpr*>(expr.get());
+    EmitLiteralExpr(p);
+    break;
+  }
+  case Expr::Kind::kBinary: {
+    auto p = static_cast<BinaryExpr*>(expr.get());
+    EmitBinaryExpr(p);
+    break;
+  }
+  case Expr::Kind::kVariable: {
+    assert(false && "还未设置变量部分！");
+    auto p = static_cast<VariableExpr*>(expr.get());
+    EmitVariableExpr(p);
+    break;
+  }
+  }
+}
+
+void Generator::EmitLiteralExpr(const LiteralExpr* expr) {
+  double num = 0;
+  for (char c : expr->lexeme) {
+    num = num * 10 + (c - '0');
+  }
+  Value v = Value::Num(num);
+  EmitConst(v);
+  return;
+}
+
+void Generator::EmitVariableExpr(const VariableExpr* expr) {
+  return;
+}
+
+void Generator::EmitBinaryExpr(const BinaryExpr* expr) {
+  EmitExpr(expr->left);
+  EmitExpr(expr->right);
+  switch (expr->op.kind) {
+    case TokenKind::kPlus: 
+      EmitOp(OpCode::OP_ADD);
+      break;
+    case TokenKind::kMinus: 
+      EmitOp(OpCode::OP_SUB);
+      break;
+    case TokenKind::kStar:
+      EmitOp(OpCode::OP_MUL);
+      break;
+    case TokenKind::kSlash:
+      EmitOp(OpCode::OP_DIV);
+      break;
+  }
+  return;
+}
+
+void Generator::EmitOp(OpCode op) {
+  current_fn_->Emit(op, 1);
+  return;
+}
+
+void Generator::EmitConst(const Value& v) {
+  int index = current_fn_->AddConst(v);
+  current_fn_->Emit(OpCode::OP_CONSTANT, 1);
+  current_fn_->EmitI32(index, 1);
+  return;
+}
+
+}  // namespace cilly
---
+++ src/frontend/generator.h
@@ -0,0 +1,48 @@
+#ifndef CILLY_VM_CPP_GENERATOR_H_
+#define CILLY_VM_CPP_GENERATOR_H_
+
+#include <memory>
+#include <vector>
+#include "ast.h"
+#include "../function.h"
+#include "../opcodes.h"
+
+
+namespace cilly {
+
+// 负责把 AST (Stmt/Expr) 生成字节码（一个 Function）
+class Generator {
+ public:
+  // 生成器不一定需要 VM，先不依赖 VM，只生成 Function。
+  Generator();
+
+  // 输入：一整棵 AST（顶层语句列表）
+  // 输出：一个可在 VM 中执行的 Function（类似 main 函数）
+  Function Generate(const std::vector<StmtPtr>& program);
+
+ private:
+  // 当前正在生成字节码的函数（例如 main）
+  Function* current_fn_;
+
+  // 工具：生成一条语句
+  void EmitStmt(const StmtPtr& stmt);
+
+  // 未来会扩展：var/if/while/for 等
+  void EmitPrintStmt(const PrintStmt* stmt);
+  void EmitExprStmt(const ExprStmt* stmt);
+
+  // 工具：生成一个表达式的字节码，把结果留在栈顶
+  void EmitExpr(const ExprPtr& expr);
+  void EmitLiteralExpr(const LiteralExpr* expr);
+  void EmitVariableExpr(const VariableExpr* expr);
+  void EmitBinaryExpr(const BinaryExpr* expr);
+  // 以后可能会有 UnaryExpr / AssignExpr 等
+
+  // 往 current_fn_ 里 emit 指令
+  void EmitOp(OpCode op);
+  void EmitConst(const Value& v);
+};
+
+}  // namespace cilly
+
+#endif  // CILLY_VM_CPP_GENERATOR_H_
